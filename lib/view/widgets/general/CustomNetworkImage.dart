4c10106f92d711971bfaa4ec927a6e34ccd27ed4
Awais Amjed <awaisamjed171@gmail.com>
2022-05-05 12:06:31 +0500

    Fix Cached Network image exception

--- lib/view/widgets/general/CustomNetworkImage.dart
+++ lib/view/widgets/general/CustomNetworkImage.dart
@@ -17,15 +17,22 @@ class CustomNetworkImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: url ?? '',
-      placeholder: (context, url) => const SizedBox(),
-      errorWidget: (context, url, error) =>
-          //TODO Add placeholder
-          Image.asset('assets/images/logo.png'),
-      height: height,
-      width: width,
-      fit: fit,
-    );
+    return url == null
+        ? CachedNetworkImage(
+            imageUrl: url ?? '',
+            placeholder: (context, url) => const SizedBox(),
+            errorWidget: (context, url, error) =>
+                //TODO Add placeholder
+                Image.asset('assets/images/logo.png'),
+            height: height,
+            width: width,
+            fit: fit,
+          )
+        : Image.asset(
+            'assets/images/logo.png',
+            height: height,
+            width: width,
+            fit: fit,
+          );
   }
 }
